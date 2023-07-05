689efb7e76e4924108a090ba93cfe9693a3c2c80
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-07-04 21:14:55 -0300

    Exercicio Finalizado

--- src/contaBancaria/Aplication.java
+++ src/contaBancaria/Aplication.java
@@ -1,6 +1,23 @@
 package contaBancaria;
 public class Aplication {
     public static void main(String[] args) {
+        ContaBanco pessoa_1 = new ContaBanco();
+        pessoa_1.setNumConta(2424);
+        pessoa_1.setDono("Maria");
+        pessoa_1.abrirConta("CC");
 
+        ContaBanco pessoa_2 = new ContaBanco();
+        pessoa_2.setNumConta(2323);
+        pessoa_2.setDono("Ayla");
+        pessoa_2.abrirConta("CP");
+
+        pessoa_1.depositar(300);
+        pessoa_2.depositar(500);
+        pessoa_1.sacar(250);
+        pessoa_2.sacar(250);
+
+
+        pessoa_1.estadoAtual();
+        pessoa_2.estadoAtual();
     }
 }
